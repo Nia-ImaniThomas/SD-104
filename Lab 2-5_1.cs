5610508e3e07a69727713e87bda3ffcb343dc31e
Nia-Imani Thomas <61117975+Nia-ImaniThomas@users.noreply.github.com>
2020-02-19 06:43:14 -0600

    Add files via upload

---
+++ Lab 2-5_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab_2_5_1_of_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 2-4_1 of 4");
+            /*1. Write a C# program to print on screen the output of adding, subtracting,
+            multiplying and dividing of two numbers which will be entered by the user.*/
+            int number1; //input one
+            int number2; //input two
+
+            Console.WriteLine("2");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("7");
+            number2 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
