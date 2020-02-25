9bb05de26e31d43fb4a6093f59693ffbca5f3292
Nia-Imani Thomas <61117975+Nia-ImaniThomas@users.noreply.github.com>
2020-02-25 12:51:14 -0600

    Finished Lab 2-5_3 of 4

---
+++ Lab 2-5_3 of 4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab_2_5_3_of_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*3. Write a C# program to print the output of multiplication of three numbers which
+              will be entered by the user*/
+            int number1; 
+            int number2;
+            int number3;
+
+            Console.WriteLine("Enter a value for the first number: 3");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a value for the second number: 6");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a value for the third number: 9");
+            number3 = Convert.ToInt32(Console.ReadLine());
+
+            int result = number1 * number2 * number3;
+            Console.WriteLine("The answer is: 162");
+            Console.WriteLine(result);
+        }
+    }
+}
