8a42bc63784d620ab9181f32dd55321cdbd6d48d
Nia-Imani Thomas <61117975+Nia-ImaniThomas@users.noreply.github.com>
2020-02-25 06:19:18 -0600

    Scenario

--- Lab 4-6 Trivia Leaderboard.cs
+++ Lab 4-6 Trivia Leaderboard.cs
@@ -7,6 +7,30 @@ namespace Lab_4_6_Trivia_Leaderboard
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            /*Scenario:
+        You work for a company that runs a trivia web site. Users of the site compete by
+        answering trivia questions to get the best score possible. One of the features of the site
+        is that users are able to see a list of high scores.
+        You’ve been asked to use C# to create the score leaderboard logic. You work for a
+        large team, so someone else will worry about getting it to display on the page, your task
+        is to implement the code logic.
+        You’ve been given these scores to work with.
+        Score
+        4400
+        8900
+        1200
+        3700
+        7200
+
+ 
+             Using C# lists, do the following.
+        1. Create a list that holds the scores you’ve been given.
+        2. Sort the scores lowest to highest and output to the screen.
+        3. Sort the scores highest to lowest and output to the screen.
+        4. You’ve just been given a new score that wasn’t part of the original list. Add the score
+        2400 to the list. 
+        5.. A user has closed their account and needs to have all of their information deleted.
+        Remove the score 4400 from the list.*/
         }
     }
 }
