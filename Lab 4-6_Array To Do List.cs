98155c6a355e8b743149f703149d94a21bd1c4e0
Nia-Imani Thomas <61117975+Nia-ImaniThomas@users.noreply.github.com>
2020-02-25 06:22:08 -0600

    Scenario

---
+++ Lab 4-6_Array To Do List.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab_4_6_Array_To_Do_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }/* Scenario:
+        You work for a company that runs a To-Do list web app. The website allows users to
+        type in tasks for each day of the week so that they remember to do them. You’ve been
+        asked to use C# to create a small to-do list planner. As usual, someone else will handle
+        how the code will work on the web, you’ve been asked to write the initial test version of
+        the logic.
+        
+        Requirements:
+        The do-to list planner with cycle through each day of the week, pausing to let the user
+        type in a task. The planner will save each task the user types in, and then display all of
+        the days and tasks together. See the screenshot below for an example of the screen
+        output.  
+        
+        Tips:
+        You will keep each day in an array of “days”.
+        You’ll save the tasks in an array of “tasks”.
+        You need to setup for each loops to loop through the days and tasks.*/
+    }
+}
