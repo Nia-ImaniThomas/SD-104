acfd49b7649ae5513f4fc0311864e8e7a37b0cdd
Nia-Imani Thomas <61117975+Nia-ImaniThomas@users.noreply.github.com>
2020-02-25 12:54:16 -0600

    Finished Lab 2-5_2 of 4

---
+++ Lab 2-5_2 of 4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab_2_5_2_of_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* 2.Write a C# program to print Hello and your name (which you will type in on the
+                Console) in a separate line.*/
+            Console.WriteLine("Hello Nia-Imani Thomas!");
+        }
+    }
+}
