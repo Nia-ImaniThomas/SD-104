51f3dd0ca8948061556505f7ffbb08057026d03f
Nia-Imani Thomas <61117975+Nia-ImaniThomas@users.noreply.github.com>
2020-02-25 06:06:08 -0600

    Add files via upload

---
+++ Lab 4-6 Trivia Leaderboard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_4_6_Trivia_Leaderboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
