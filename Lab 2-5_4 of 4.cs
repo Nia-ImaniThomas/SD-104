d769302114a960421fc9f9600e00546e63b5b5ac
Nia-Imani Thomas <61117975+Nia-ImaniThomas@users.noreply.github.com>
2020-02-25 13:22:01 -0600

    Finished Lab 2-5_4 of 4

---
+++ Lab 2-5_4 of 4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_2_5_4_of_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*4. Write a C# program that takes an age (for example 20) as input and prints
+something like "You look younger than 20 (the age the user entered)"*/
+            int number = 28;
+
+            int result = number;
+            Console.WriteLine("You look younger than");
+            Console.WriteLine(result);
+
+        }
+    }
+}
