7050de6c7e97653ad3144468a9a6dabfd392064e
Nia-Imani Thomas <61117975+Nia-ImaniThomas@users.noreply.github.com>
2020-03-23 04:33:21 -0500

    Lab 5-6_If_Statement

---
+++ Lab 5-6_If_Statements.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Lab_5_6_If_Statement
+{
+    class Program
+    {
+       public struct Customer
+        {
+            public string first_name;
+            public string last_name;
+            public int age;
+            public bool hasMembership;
+        }
+
+        static void Main(string [] args)
+        {
+
+
+            Customer customer1 = new Customer();
+            Customer customer2 = new Customer();
+            Customer customer3 = new Customer();
+
+            customer1.first_name = "Mike";
+            customer1.last_name = "Smith";
+            customer1.age = 17;
+            customer1.hasMembership = false;
+
+
+            customer2.first_name = "John";
+            customer2.last_name = "White";
+            customer2.age = 43;
+            customer2.hasMembership = false;
+
+
+            customer3.first_name = "Harold";
+            customer3.last_name = "Marcus";
+            customer3.age = 22;
+            customer3.hasMembership = true;
+
+            Customer[] allCustomers = { customer1, customer2, customer3 };
+
+            for(int i = 0; i <allCustomers.Length; i++)
+            {
+                if (allCustomers[i].age < 21)
+                {
+                    Console.WriteLine(allCustomers[i].first_name + " " + allCustomers[i].last_name + " is not allowed to place an order");
+
+                }
+                else
+                {
+                    Console.WriteLine(allCustomers[i].first_name + " " + allCustomers[i].last_name + "can place an order");
+
+                }
+                if (allCustomers[i].hasMembership == true)
+                {
+                    Console.WriteLine(allCustomers[i].first_name + " " + allCustomers[i].last_name + "is a premium member.");
+
+                }
+                else
+                {
+                    Console.WriteLine(allCustomers[i].first_name + " " + allCustomers[i].last_name + "is standard member. ");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,91 +1,21 @@
 ﻿using System;
 
-namespace Lab_5_4_Mario
+namespace Lab_5_6_If_Statement
 {
     class Program
     {
-        private static object mario;
-
-        public struct Mario
+       public struct Customer
         {
-            /*Scenario:
-You work for a company that allows users to play classic video games on their web site.
-You’ve been asked to re-create the classic character Mario for use on the website.
-Using a struct, create various data points that makeup Mario.*/
-
-
-           public enum Size
-            {
-                small,
-                large
-            }
-            /*1. We need to begin by thinking of all of things that Mario might need. Let’s start
-                 making a list. 
-
-                 Here’s the list we came up with.
-                 Number of lives
-                 Number of gold coins
-                 Current Stage
-                 Size (small or large)
-                 Can Shoot a fireball? (yes or no)
-                 Is jumping right now ( yes or no)  */
-            /*2. Now, let’s turn this into a struct. This code is written ABOVE the Main() Code
-                 Block. Do not place it inside of it.   */
-
-
-            public int numOfLives;
-            public string currentLevel;
-            public Size sizeOfMario;
-            public bool canShootFireBalls;
-            public bool isJumping;
-            public int numOfCoins;
-            internal static Mario mario;
+            public string first_name;
+            public string last_name;
+            public int age;
+            public bool hasMembership;
         }
 
-        /*3. Now in the Main() code block, let’s create Mario and give him some default values. 
-         * Let’s play some Super Mario in C#
-           Open the 5_4 LabStarter, read through the code. Change the values of Mario as
-           directed as he moves through “the level.”*/
-           static void Main(string[] arges)
+        static void Main(string [] args)
         {
-            Mario.mario = new Mario();
-            mario.numOfLives = 3;
-            mario.currentLevel = "World 1-1";
-            mario.sizeOfMario = Mario.Size.small;
-            mario.canShootFireBalls = false;
-            mario.isJumping = false;
-            mario.numOfCoins = 0;
 
-
-
-            Console.WriteLine("Mario is running through the level and meets a Gooba. Set Mario to jummping so he can kill it");
-            //Write code here
-            mario.isJumping = true;
-
-            Console.WriteLine("Mario squashes the Goobs. Now he needs to land!");
-            //Write code here
-            mario.isJumping = false;
-
-            Console.WriteLine("Mario hits a block and finds a 1UP. Increase Mario's life by one.");
-            //Write code here
-            mario.numOfLives += 1;
-
-            Console.WriteLine("Mario finds a secret Fire Flower and can now shoot fireballs. Change Mario!");
-            //Write code here
-            mario.canShootFireBalls = true;
-            
-            Console.WriteLine("Mario finds another power up mushroom and gets big. Change Mario!");
-            //Write code here
-            mario.sizeOfMario = Mario.Size.large;
-            
-            Console.WriteLine("Mario finds a stash of gold coins. Give Mario 10 coins.");
-            //Write code here
-            mario.numOfConis = 10;
-
-            Console.WriteLine("Mario finshes the level! Change Mario's current level to World 1-2");
-            //Write code here
-            mario.currentLevel = "World 1-2";
-
+            Customer customer1 = new Customer();
 
         }
     }
