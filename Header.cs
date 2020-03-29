b4cf32c80ea6f4305fe0baf333a37a865842e998
Nia-Imani Thomas <61117975+Nia-ImaniThomas@users.noreply.github.com>
2020-03-29 04:14:09 -0500

    Lab 9_Creating Dynamic HTML

---
+++ Header.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_9_4_HTML
+{
+    class Header : Program_Lab9_4_HTML
+    {
+        public const string open = "<h1> " +
+            "Cake Tasting Options";
+        public const string close = "</h1>";
+
+        public string CreateHeader(string text)
+        {
+            string header = String.Concat(open, text, close, "\n");
+            return header;
+        }
+    }
+
+    class UnorderedList
+    {
+        public const string open = "<ul>" +
+            "Tasting for 2, " +
+            "Tasting for 3, " +
+            "Tasting for 4";
+        public const string close = "</ul>";
+
+        public string CreateListItem(string text)
+        {
+            string open = "<li>\n";
+            string close = "</li>\n";
+
+            string listItem = String.Concat(open, text, close, "\n");
+            return listItem;
+        }
+
+        public StringBuilder CreateList(string[] listItems)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(open);
+            foreach (string item in listItems)
+            {
+                sb.Append(item);
+            }
+            sb.Append(close);
+
+            return sb;
+        }
+
+    }
+}
---
+++ Program_Lab9_4_HTML.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab_9_4_HTML
+{
+    class Program_Lab9_4_HTML
+    {
+        static void Main(string[] args)
+        {
+            string filename = " C:\\Weblogs\\Lab 9 - 4_HTML\\Lab 9 - 4_HTML";
+
+            Header header = new Header();
+            UnorderedList list = new UnorderedList();
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine("Enter text for HTML header.");
+            string headerElement = header.CreateHeader(Console.ReadLine());
+            string[] listItems = new string[3];
+
+            for (int i = 0; i < listItems.Length; i++)
+            {
+                Console.WriteLine("Add another item to the list.");
+                listItems[i] = list.CreateListItem(Console.ReadLine());
+            }
+
+            StringBuilder listElement = list.CreateList(listItems);
+            sb.Append(headerElement);
+            sb.Append(listElement.ToString());
+
+            File.AppendAllText(filename, sb.ToString());
+
+        }
+    }
+}
